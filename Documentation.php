7262c75e0eaa90e114304a926c9f999281580771
Mahmod Mashlah <mashlahmahmod@gmail.com>
2023-05-29 00:01:38 +0300

    Jetstream for Admin + Make Database (.env) + Documentation.php File

---
+++ Documentation.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Jetstreams :
+
+composer require laravel/jetstream
+
+Install Jetstream With Livewire :                                  //Or, Install Jetstream With Inertia  (that i didnt do)//Or, Install Jetstream With Inertia  (that i didnt do)
+php artisan jetstream:install livewire // i code this                        //php artisan jetstream:install inertia
+// or : php artisan jetstream:install livewire --teams                        //php artisan jetstream:install inertia --teams
+
+// or to do the Dark Mode :
+
+    php artisan jetstream:install livewire --dark
+
+//////
+
+npm install
+
+npm run build
+
+php artisan migrate
+
+npm run dev
+
+php artisan serve
+
+ */
+
+
+?>
