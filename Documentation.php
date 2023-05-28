fd66740a6cbb324fecc67a4ab64709dcfd3c23ae
Mahmod Mashlah <mashlahmahmod@gmail.com>
2023-05-29 00:36:32 +0300

    Auth Api Routes (api.php)

--- Documentation.php
+++ Documentation.php
@@ -146,8 +146,38 @@ class AuthController extends Controller
 }
 
 /////////////////////////////////////
+* Auth Api Routes (api.php) :
+    go to api.php  :
+
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// public Routes
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+// protected Routes (With Auth)
+
+// Route::prefix()-> group(['middleware'=>['auth:sanctum']],function () {} //to implement prefix
+
+Route::group(['middleware'=>['auth:sanctum']],function () {
+
+Route::resource('/tasks', TaskController::class);
+Route::post('/logout', [AuthController::class,'logout']);
+
+});
+
+/////////////////////////////////////
 
  */
 
