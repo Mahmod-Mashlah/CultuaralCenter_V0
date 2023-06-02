e062347545414a4a90aa74e942cca219bef26333
Mahmod Mashlah <mashlahmahmod@gmail.com>
2023-06-02 15:46:23 +0300

    Lecture controller api (functions + requests + responces) WITHOUT Auth

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -5,62 +5,86 @@ namespace App\Http\Controllers;
 use App\Models\Lecture;
 use App\Http\Requests\StoreLectureRequest;
 use App\Http\Requests\UpdateLectureRequest;
-
+use App\Http\Resources\LecturesResource;
+use App\Traits\HttpResponse;
+use Illuminate\Support\Facades\Auth;
 class LectureController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    use HttpResponse ;
+
     public function index()
     {
-        //
+        #  return LecturesResource::collection(
+        #   Lecture::where('user_id',Auth::user()->id)->get() ) ; // get lectures thats users are authenticated
+
+        // return LecturesResource::collection(
+        //     Lecture::where('user_id',Auth::user()->id)->get() ) ; // get lectures thats users are authenticated
+        return LecturesResource::collection(Lecture::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreLectureRequest $request)
-    {
-        //
+     {
+        $request -> validated($request->all() );
+
+        $lecture = Lecture::create([
+
+            // 'user_id' => Auth::user()->id ,
+
+            'name'               => $request-> name,
+            'type'               => $request-> type,
+            'start_date'         => $request-> start_date,
+            'start_time'         => $request-> start_time,
+            'end_time'           => $request-> end_time,
+            'target_people'      => $request-> target_people,
+            'teacher_experience' => $request-> teacher_experience,
+        ]);
+
+        return new LecturesResource($lecture) ;
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Lecture $lecture)
     {
-        //
+        return new LecturesResource($lecture);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lecture $lecture)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateLectureRequest $request, Lecture $lecture)
     {
-        //
+        // this work correctly
+        // in postman make the method : Post (not patch not put)
+        // and make in request body : _method = PUT
+
+        $lecture=Lecture::find($lecture->id);
+        // if (Auth::user()->id !== $lecture->user_id) {
+        //     return $this->error('','You are not Authorized to make this request',403);
+        //     // you should use the trait   (use HttpResponses ;) above
+
+        // }
+
+        $lecture->update($request->all());
+        $lecture->save();
+
+        return new LecturesResource($lecture);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Lecture $lecture)
     {
-        //
+        // way 1 :
+            $lecture->delete();
+            return $this->success('Lecture was Deleted Successfuly ',null,204);
+
+        // way 2 : (it is best to do it in Show & Update functions [Implement Private function below] )
+
+        // return $this->isNotAuthorized($lecture) ? $this->isNotAuthorized($lecture) : $lecture->delete();
+        // return true (1) if the delete successfuly occoured
     }
+
+    private function isNotAuthorized($lecture)
+    {
+        if (Auth::user()->id !== $lecture->user_id) {
+            return $this->error('','You are not Authorized to make this request',403);
+            }
+        }
 }
