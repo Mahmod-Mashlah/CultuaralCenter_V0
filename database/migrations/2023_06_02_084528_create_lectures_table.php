3ceb72afbb97a064ec98e22a2445953bead11ced
Mahmod Mashlah <mashlahmahmod@gmail.com>
2023-06-02 12:07:29 +0300

    Lecture Migration (without Relationships)

--- database/migrations/2023_06_02_084528_create_lectures_table.php
+++ database/migrations/2023_06_02_084528_create_lectures_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('lectures', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('target_people');
+            $table->string('teacher_experience');
+
+            // $table->string('priority')->default('medium');
+
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->timestamps();
         });
