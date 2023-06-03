6dbec7ec95e726095c225190ae5424d2986ff66a
Mahmod Mashlah <mashlahmahmod@gmail.com>
2023-06-03 14:04:30 +0300

    Play api Routes 2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LectureController;
+use App\Http\Controllers\PlayController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
