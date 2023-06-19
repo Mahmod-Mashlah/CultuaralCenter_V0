8f9df3a8b85d583da740f739bcf2829fb13818cf
Mahmod-Mashlah <mashlahmahmod@gmail.com>
2023-06-19 03:56:11 +0300

    make body template (content-template.blade.php)

---
+++ resources/views/web/content-template.blade.php
@@ -0,0 +1,31 @@
+@extends('web.layouts.master')
+
+@section('title')
+    {{-- Dashboard || Home Page --}}
+@endsection {{-- or @stop --}}
+
+@section('css')
+
+@endsection
+
+@section('root')
+    {{-- Dashboard --}}
+@endsection
+
+@section('son1')
+    {{-- Plans --}}
+@endsection
+
+@section('son2')
+
+@endsection
+
+@section('content')
+
+
+
+@endsection
+
+@section('javascript')
+
+@endsection
