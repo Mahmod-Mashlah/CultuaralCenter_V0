938f830404e98909cf1699e5551eaa597df7809a
Mahmod-Mashlah <mashlahmahmod@gmail.com>
2023-07-06 16:02:01 +0300

    Department Searchfunctions

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Department;
+use App\Traits\HttpResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\DepartmentsResource;
 use App\Http\Requests\StoreDepartmentRequest;
 use App\Http\Requests\UpdateDepartmentRequest;
-use App\Traits\HttpResponse;
-use App\Http\Resources\DepartmentsResource;
-use Illuminate\Support\Facades\Auth;
+
 class DepartmentController extends Controller
 {
     use HttpResponse ;
@@ -20,6 +22,28 @@ class DepartmentController extends Controller
         #   Department::where('user_id',Auth::user()->id)->get() ) ; // get departments thats users are authenticated
 
         return DepartmentsResource::collection(Department::get()) ; // get departments thats users are authenticated
+
+    }
+
+    public function search(Request $request)
+    {
+        $query = Department::query();
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+
+        if ($request->has('rows_count')) {
+            $query->where('rows_count', 'like', '%' . $request->input('rows_count') . '%');
+        }
+
+        if ($request->has('max_row_books')) {
+            $query->where('max_row_books', '=', $request->input('max_row_books') );
+        }
+
+        $department = $query->get();
+        return $this->success($department);
 
     }
     public function store(StoreDepartmentRequest $request)
